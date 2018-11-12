39383c4bd09f485cd1be0ac6540e4d5be20c69db
Bao Phung <bao.future94@gmail.com>
2018-11-12 13:27:35 +0700

    complete

---
+++ client/precache-manifest.c50cedaaf8a9e55015402bfe6f5f4488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52a7ff6d38968a229f38",
+    "url": "/static/js/main.52a7ff6d.chunk.js"
+  },
+  {
+    "revision": "f147411f3c2fb64daf64",
+    "url": "/static/js/1.f147411f.chunk.js"
+  },
+  {
+    "revision": "52a7ff6d38968a229f38",
+    "url": "/static/css/main.d52c482c.chunk.css"
+  },
+  {
+    "revision": "8432b11429530930c547dc335c965d04",
+    "url": "/index.html"
+  }
+];
--- client_src/src/components/Main.js
+++ client_src/src/components/Main.js
@@ -4,13 +4,16 @@ import Meetups from './Meetups';
 import About from './About';
 import MeetupDetails from './MeetupDetails';
 import AddMeetup from './AddMeetup';
+import EditMeetup from './EditMeetup';
+
 const Main = () => (
     <main>
         <Switch>
             <Route exact path='/' component={Meetups} />
             <Route exact path='/about' component={About} />
-            <Route exect path='/meetups/add' component={AddMeetup} />
-            <Route exec path='/meetups/:id' component={MeetupDetails} />
+            <Route exact path='/meetups/add' component={AddMeetup} />
+            <Route exact path='/meetups/edit/:id' component={EditMeetup} />
+            <Route exact path='/meetups/:id' component={MeetupDetails} />
         </Switch>
     </main>
 )
